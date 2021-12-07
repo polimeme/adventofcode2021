6110190b8c519067488fbca44fccdd15e71e1905
vlad <64670363+polimeme@users.noreply.github.com>
2021-12-07 21:40:32 +0100

    Update AOC21_day7.py

--- AOC21_day7.py
+++ AOC21_day7.py
@@ -1,5 +1,5 @@
 from math import sqrt
-l=input()
+l=[]
 def devstandard(l):
     m=sum(l)/len(l)
     a=0
@@ -13,17 +13,20 @@ def summa(s,n): #TOO MUCH RECURSIVE FOR PY
         return s
 def wayne(l):
     r = []
+    g = []
     for c in range(int((sum(l)/len(l))-devstandard(l)),int((sum(l)/len(l))+devstandard(l))):
         a = 0
+        b = 0
         for elem in l:
             #a=summa(a,abs(elem-c)+1)
             #print(abs(elem-c),summa(0,abs(elem-c)+2))
+            b+=abs(elem-c)
             for k in range(1, abs(elem - c) + 1):
                 a += k
 
         r.append(a)
-    return min(r)
-print(wayne(l))
+        g.append(b)
 
-
-
+    return min(r), min(g)
+out=(wayne(l))
+print('PT1:',out[0],'\nPT2:',out[1])
