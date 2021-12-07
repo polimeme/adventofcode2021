35277bd10073de86b445a606509aa34bf8281ee1
vlad <64670363+polimeme@users.noreply.github.com>
2021-12-07 15:00:08 +0100

    Update and rename shi.py to AOC21_day6.py

--- AOC21_day6.py
+++ AOC21_day6.py
@@ -20,20 +20,14 @@ def shi(h,i):
                     l[k]-=1
         r.append(l)
     print(len(r))
-    #h=[h]
     f=[]
     a=0
     for n in range(1):
-        #h.append([])
         f=[]
         print(n)
         for k in range(len(h)):
-            #h[-1]=list((h[-1]+r[h[-2][k]]))
             f=f+r[h[k]]
-            #print(h[-1])
-            #h.remove(h[k])
         print(len(f))
-        #h=h[-2:]
         h=f
         del(f)
         a=len(h)
@@ -43,34 +37,7 @@ def shi(h,i):
 
 
     return a
-#a=[1,2,3]
-##b=[4,7,6]
-#print(list(a+b))
 
 print(shi(h,128))
-'''
-a=[2,[3,5,[6,7],[5,3]],[0,43],[5,7]]
-from itertools import chain
-#print((list(chain.from_iterable(a))))
-def iter(subl,l):
-    for elem in subl:
-        if type(elem)!=list:
 
-            l.append(subl)
-        else:
-            #for elem in subl:
-                l=iter(elem,l)
-    return l
-def iter2(l):
-    
-
-    for elem in l:
-        if type(elem)==int:
-            h.append(elem)
-            l.remove(elem)
-            l=(list(chain.from_iterable(a)))
-    for elem in h:
-        l.append(0)
-    return l
-'''
 
